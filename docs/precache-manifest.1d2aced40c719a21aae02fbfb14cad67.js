eeede7bda05864c16c90a158016100c1f4245aea
Alain Hernandez <alain@flyzipline.com>
2018-10-12 00:24:01 -0700

    Add docs as github source

---
+++ docs/precache-manifest.1d2aced40c719a21aae02fbfb14cad67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a09f3c288ca7738008663eb8414bef2b",
+    "url": "/static/media/swrpg.a09f3c28.otf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc2007d28ad55da72c25",
+    "url": "/static/js/main.dc2007d2.chunk.js"
+  },
+  {
+    "revision": "5b6703a2d9d5f48dafc4",
+    "url": "/static/js/1.5b6703a2.chunk.js"
+  },
+  {
+    "revision": "dc2007d28ad55da72c25",
+    "url": "/static/css/main.5312f670.chunk.css"
+  },
+  {
+    "revision": "77a7f6129c601fcac141ab58db1b4e1e",
+    "url": "/index.html"
+  }
+];
